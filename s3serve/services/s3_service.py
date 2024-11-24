ef5b39317354ccbff83cad952a45b24f553ff6ef
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-11-24 23:10:51 +0100

    ci: 💚 Fix tests

--- s3serve/services/s3_service.py
+++ s3serve/services/s3_service.py
@@ -1,16 +1,29 @@
 import logging
+from typing import Union
 
 import boto3
 from botocore.exceptions import ClientError
+from mypy_boto3_s3 import S3Client
 
 from s3serve.schemas.ListObjectsV2Response import ListObjectsV2Response
 
-s3 = boto3.client('s3')
+_s3_client: Union[None, S3Client] = None
+
+
+def s3_client() -> S3Client:
+    if _s3_client is None:
+        _init_s3_client()
+    return _s3_client
+
+
+def _init_s3_client():
+    global _s3_client
+    _s3_client = boto3.client('s3')
 
 
 def get_buckets() -> list[dict]:
     try:
-        res = s3.list_buckets(MaxBuckets=1000)
+        res = s3_client().list_buckets(MaxBuckets=1000)
         return res['Buckets']
     except ClientError as e:
         logging.error(e)
@@ -20,9 +33,9 @@ def get_buckets() -> list[dict]:
 def get_objects(bucket: str, prefix: str = None) -> ListObjectsV2Response:
     try:
         if prefix:
-            res = s3.list_objects_v2(Bucket=bucket, Prefix=prefix, MaxKeys=25, Delimiter='/')
+            res = s3_client().list_objects_v2(Bucket=bucket, Prefix=prefix, MaxKeys=25, Delimiter='/')
         else:
-            res = s3.list_objects_v2(Bucket=bucket, MaxKeys=25, Delimiter='/')
+            res = s3_client().list_objects_v2(Bucket=bucket, MaxKeys=25, Delimiter='/')
         return ListObjectsV2Response(**res)
     except ClientError as e:
         logging.error(e)
